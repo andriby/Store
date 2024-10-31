9cf8ac343729b9062958cce6f76f8fe8e7edcbb9
Adrian <adrian.dryzara3@gmail.com>
2024-10-30 21:10:00 -0400

    Creacion de componentes y llamada de productos (basico)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './domains/products/components/product/product.component';
+import { ListComponent } from './domains/products/pages/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListComponent
+    }
+];
